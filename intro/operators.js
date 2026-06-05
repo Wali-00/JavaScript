299b1a1b489cd7f3489b5265af13fbed1fef4b7c
Wali-00 <waliu4574@gmail.com>
2026-06-06 03:38:12 +0600

    arithmetic operators

---
+++ intro/operators.js
@@ -0,0 +1,12 @@
+/*
+var a = 10;
+var b = 20;
+var sum = a+b; // additional operator
+var difference = a-b; // subtraction operator
+var product = a*b; // multiplication operator
+var quotient = a/b; // division operator
+var remainder = a%b; // modulus operator
+
+var a =a+5; // this is the same as a = a + 5; this is called the addition assignment operator
+console.log(a); // this will print 15
+*/ 
