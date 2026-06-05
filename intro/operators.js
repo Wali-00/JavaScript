948a97ea13cabb8c81f38f45aadad7e3e2889057
Wali-00 <waliu4574@gmail.com>
2026-06-06 03:45:12 +0600

    shorthand and concat

--- intro/operators.js
+++ intro/operators.js
@@ -9,4 +9,10 @@ var remainder = a%b; // modulus operator
 
 var a =a+5; // this is the same as a = a + 5; this is called the addition assignment operator
 console.log(a); // this will print 15
+
+var a+=5; // this is the same as a = a + 5; this is called the addition assignment operator
+
+console.log(40+'5'); // this will print 405 because the number 40 is converted to a string and concatenated with the string '5'
+console.log(40-'5'); // this will print 35 because the string '5' is converted to a number and subtracted from the number 40
+string just concatenation works with the + operator, other operators will try to convert the string to a number and perform the operation
 */ 
