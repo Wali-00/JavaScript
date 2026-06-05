20c7fb0921ec4233e2e5a468e9a3341f7659baf7
Wali-00 <waliu4574@gmail.com>
2026-06-05 12:05:34 +0600

    variable data types

---
+++ intro/types.js
@@ -0,0 +1,24 @@
+var name = "wali";
+var address = "karachi";
+var city = 'karachi';
+console.log(name);
+console.log(address);
+console.log(city);
+console.log(typeof name);
+
+// these are string data types
+//we can use single or double quotes to define a string
+
+
+var isRich = true;
+var isPoor = false;
+console.log(isRich);
+console.log(isPoor);
+console.log(typeof isRich);
+// these are boolean data types
+// type of operator is used to find the data type of a variable
+
+var age = 22;
+console.log(age);
+console.log(typeof age);
+// these are number data types
---
+++ intro/variable.js
@@ -0,0 +1,15 @@
+// 5 things to declare a variable in JavaScript
+// 1. var
+// 2. let
+// 3. const
+// 4. global variable
+// 5. local variable
+
+var weight = 30;
+var price = 1000;
+var money = 5000;
+money = 6000;
+
+console.log(weight);
+console.log(price);
+console.log(money);
