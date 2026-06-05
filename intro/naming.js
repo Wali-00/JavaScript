0b7846554dea8926be629015164843b0d0243359
Wali-00 <waliu4574@gmail.com>
2026-06-06 03:26:03 +0600

    variable name naming convention

---
+++ intro/naming.js
@@ -0,0 +1,33 @@
+/*
+ we cannot use keywords as variable names
+var var = 5; // This will cause a syntax error
+
+we cannot start variable names with numbers
+var 1name = "wali"; // This will cause a syntax error
+
+we cannot use special characters (except for $ and _) in variable names
+var name@ = "wali"; // This will cause a syntax error
+
+we cannot use spaces in variable names
+var first name = "wali"; // This will cause a syntax error
+
+we cannot use quotes in variable names
+var "name" = "wali"; // This will cause a syntax error
+
+name is case sensitive
+var Name = "wali";
+var name = "wali";
+console.log(Name); // This will print "wali"
+console.log(name); // This will also print "wali"
+
+how to write a long variable name
+var firstName = "wali"; //camel case notation
+var first_name = "wali"; //snake case notation
+var FirstName = "wali"; //pascal case notation
+
+also we cannot use reserved words as variable names
+var if = "wali";
+
+keywords all are small letters
+
+*/ 
