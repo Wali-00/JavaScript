e6edabd740a424cc459f8246da79bdc8e4bbef26
Wali-00 <waliu4574@gmail.com>
2026-06-06 22:39:06 +0600

    comparison

---
+++ fundamental concepts of conditionals/comparison.js
@@ -0,0 +1,13 @@
+/*
+1. bigger than >
+2. smaller than <
+3. bigger than or equal to >=
+4. smaller than or equal to <=
+5. equal to ==
+6. not equal to !=
+7. strict equal to ===
+8. strict not equal to !==
+9. logical AND &&
+10. logical OR ||
+11. logical NOT !
+*/
