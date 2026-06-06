cac90646f0706bd9ff2b596dfcb857fd389d01fe
Wali-00 <waliu4574@gmail.com>
2026-06-06 22:48:45 +0600

    if else condition

---
+++ fundamental concepts of conditionals/if_else.js
@@ -0,0 +1,21 @@
+/* 
+if else structure:
+
+if (condition) {
+    // code to be executed if condition is true
+} 
+    
+else {
+    // code to be executed if condition is false
+}
+
+*/
+
+let age = 18;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+
+else{
+    console.log("You are a minor.");
+}
