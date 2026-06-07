c63d7a502ac2b2ae40a243b2845fa1df10b3a041
Wali-00 <waliu4574@gmail.com>
2026-06-07 22:04:08 +0600

    loop and while loop

---
+++ fundamental concepts of loop/loop.js
@@ -0,0 +1,12 @@
+const num = [1, 2, 3, 4, 5];
+// for loop
+
+for(number of num){
+    console.log(number);
+}
+// for...of loop is used to iterate over the elements of an iterable object (like an array). It allows you to access each element directly without needing to use an index. In this example, we loop through the num array and log each number to the console. The output will be:
+// 1
+// 2
+// 3
+// 4
+// 5
---
+++ fundamental concepts of loop/while.js
@@ -0,0 +1,10 @@
+let num =0;
+while(num<5){
+    console.log(num);
+    num++;
+}// while loop is used to execute a block of code repeatedly as long as a specified condition is true. In this example, we initialize num to 0 and use a while loop to log the value of num to the console and then increment it by 1. The loop continues until num is no longer less than 5. The output will be:
+// 0
+// 1
+// 2
+// 3
+// 4
