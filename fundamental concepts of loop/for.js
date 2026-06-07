5cec149e5dcf814f4fe68c267dc780a669bb8048
Wali-00 <waliu4574@gmail.com>
2026-06-07 22:11:34 +0600

    for loop

---
+++ fundamental concepts of loop/for.js
@@ -0,0 +1,10 @@
+//for loop structure
+// for (initialization; condition; increment/decrement) {
+//     // code to be executed
+// }
+
+const num = [1, 2, 3, 4, 5];
+
+for(let i=0; i<num.length; i++){
+    console.log(num[i]);
+}
