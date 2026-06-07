35e9a753316e5c355e9d95284f6d49103a7bf2d2
Wali-00 <waliu4574@gmail.com>
2026-06-07 22:17:54 +0600

    do while loop

---
+++ fundamental concepts of loop/do_while.js
@@ -0,0 +1,10 @@
+let num = 10;
+do {
+    console.log(num);
+    num++;
+} while(num < 15); // do...while loop is similar to a while loop, but it guarantees that the code block will be executed at least once, even if the condition is false. In this example, we initialize num to 10 and use a do...while loop to log the value of num to the console and then increment it by 1. The loop continues until num is no longer less than 15. The output will be:
+// 10
+// 11
+// 12
+// 13
+// 14
