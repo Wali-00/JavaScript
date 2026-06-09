39fe17c4fddf29190fdc59a98bae8a3d7e84726d
Wali-00 <waliu4574@gmail.com>
2026-06-09 21:50:11 +0600

    function parameter

---
+++ function/parameter.js
@@ -0,0 +1,14 @@
+
+function square(value){
+    console.log(value * value);
+}
+
+square(5); // output: 25
+
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+add(3, 4); // output: 7
