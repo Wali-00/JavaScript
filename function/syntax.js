4ccd01d16e08965847520f5ef39357632a397633
Wali-00 <waliu4574@gmail.com>
2026-06-09 21:45:22 +0600

    function syntax

---
+++ function/syntax.js
@@ -0,0 +1,15 @@
+// structure of function declaration
+
+/*
+ function functionName(){
+    code to be executed;
+ }
+*/
+
+// declaring a function
+function myFunction(){
+    console.log("Hello World");
+}
+
+// calling a function
+myFunction(); // output: Hello World
