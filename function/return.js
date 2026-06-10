abc6ddabec252e505c2426a397f38f9b07ece252
Wali-00 <waliu4574@gmail.com>
2026-06-10 19:59:06 +0600

    conditional return

--- function/return.js
+++ function/return.js
@@ -7,3 +7,14 @@ function tenTimes(num) {
 
 const output = tenTimes(5);
 console.log(output); // This will print 50
+
+
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isEven(4)); // This will print true
+console.log(isEven(7)); // This will print false
