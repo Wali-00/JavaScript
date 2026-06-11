923d3f4d475125fc8a024fd80a51cd6391725790
Wali-00 <waliu4574@gmail.com>
2026-06-11 10:04:12 +0600

    uses of math,max,min,floor,ceil,round,random

---
+++ problem solving/math.js
@@ -0,0 +1,12 @@
+const min = Math.min(10,20,30,-40);
+console.log(min); // print the smallest value
+
+const max = Math.max(10,20,30,-40);
+console.log(max); // print the largest value
+
+console.log(Math.abs(-10)); // print always positive value
+console.log(Math.round(3.5)); // if the fraction value less then 5 then it print the same decimal value but if the fraction value grater then or equal to 5 it will print upper decimal value
+console.log(Math.floor(2.99)); // always print the same decimal value
+console.log(Math.ceil(2.1)); // always print the upper decimal value
+console.log(Math.random()) // generate a random number
+console.log(Math.random()*10) // generate a random number into 10
